394414f8c2a03646f66116de2bfa42ed9effda5c
Xander Bylo <92520776+xanderbylo@users.noreply.github.com>
2022-11-05 22:11:53 +0000

    Add home subtract function

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 let homeScore = 0;
+let guestScore = 0;
 
-let homeEl = document.getElementById("home-el")
+let homeEl = document.getElementById("home-el");
+let guestEl = document.getElementById("guest-el");
+
+function homeSubtractOne() {
+    homeScore -= 1;
+    homeEl.textContent = homeScore;
+}
 
 function homeAddOne() {
     homeScore += 1;
